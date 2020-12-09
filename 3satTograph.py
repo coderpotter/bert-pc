1fad80bac3f65864c5adc8dc8ff0b0da5ba7376e
Animesh Nighojkar <animesh.nighojkar@gmail.com>
2020-12-09 15:50:48 -0500

    Create 3satTograph.py
    Reads a cnf file and creates a graph file corresponding to it which can then be used by SegBERT.

---
+++ 3satTograph.py
@@ -0,0 +1,59 @@
+op = open('segbert/graph-dataset/satlib-graphs/data.txt', 'w+')
+op.writelines('200\n')
+
+dataset = '/raid/home/animesh/storage/bert-pc/satlib/uf250-1065/uf250-0'
+for i in range(1, 101):
+    cnf = dataset+str(i)+'.cnf'
+    with open(cnf, 'r') as f:
+        for l in f.readlines():
+            if l[0] == 'c':
+                continue
+            l = l.strip().split()
+            if not l:
+                continue
+            if l[0] == 'p':
+                n_literals, n_clauses = int(l[2]), int(l[3])
+                op.writelines(str(n_clauses + 2*(n_literals)) + ' 1\n')
+                continue
+            l = list(map(int, l[:-1]))
+            if not l:
+                continue
+            line = [0]
+            for _ in l:
+                if _ < 0:
+                    line.append(n_literals + abs(_))
+                else:
+                    line.append(_)
+            op.writelines(' '.join(list(map(str, line)))+'\n')
+        for _ in range(1, n_literals+1):
+            op.writelines('0 '+str(_)+'\n')
+        for _ in range(1, n_literals+1):
+            op.writelines('0 '+str(n_literals+_)+'\n')
+dataset = '/raid/home/animesh/storage/bert-pc/satlib/uuf250-1065/uuf250-0'
+for i in range(1, 101):
+    cnf = dataset+str(i)+'.cnf'
+    with open(cnf, 'r') as f:
+        for l in f.readlines():
+            if l[0] == 'c':
+                continue
+            l = l.strip().split()
+            if not l:
+                continue
+            if l[0] == 'p':
+                n_literals, n_clauses = int(l[2]), int(l[3])
+                op.writelines(str(n_clauses + 2*(n_literals)) + ' 0\n')
+                continue
+            l = list(map(int, l[:-1]))
+            if not l:
+                continue
+            line = [0]
+            for _ in l:
+                if _ < 0:
+                    line.append(n_literals + abs(_))
+                else:
+                    line.append(_)
+            op.writelines(' '.join(list(map(str, line)))+'\n')
+        for _ in range(1, n_literals+1):
+            op.writelines('0 '+str(_)+'\n')
+        for _ in range(1, n_literals+1):
+            op.writelines('0 '+str(n_literals+_)+'\n')
